bc44eb82eb0f4ba8518e6caca77b69fe06a3b40c
Asad Chaudhary <asad.chaudhary@vmdomain.com>
2024-07-03 21:07:45 +0530

    css done

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import HeaderSidebar from '../Components/HeaderSidebar';
 import './Dashboard.css'; // Assuming you still need this for other styles
-import Sidebar from '../Components/DashComp';
+import DashComp from '../Components/DashComp';
 
 const Dashboard = () => {
     return (
         <>
         <div >
             <HeaderSidebar />
-            <Sidebar/>
+            <DashComp />
         </div>
         </>
     )
